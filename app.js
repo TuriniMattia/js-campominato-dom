34ecbeb05a72ac1ccc1ec9838ca826b2c1d1e875
TuriniMattia <turinimattia96@gmail.com>
2024-03-20 21:14:29 +0100

    [add]cliccare una casella 2 volte

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const pressBtn = document.getElementById(playGame)
 const gridEl = document.querySelector(".grid")
 console.log(gridEl)
 const levelSelectEl = document.getElementById("level")
+
+
 // let valueEasy = document.getElementById("easy").value;
 // let valueMedium = document.getElementById("medium").value;
 // let valueHard = document.getElementById("hard").value;
@@ -11,24 +13,26 @@ const levelSelectEl = document.getElementById("level")
 playGame.addEventListener('click', readyP1)
 
 function readyP1() {
-    
+
     const size = getSize()
     const numOfGridIt = size ** 2
     gridEl.innerHTML = ""
 
     const bomb = []
-    while(bomb.length < 16){
-       const numBomb = parseInt(Math.random() * numOfGridIt) + 1
-       if(bomb.includes(numBomb) === false){
-        bomb.push(numBomb)
-       }
-       console.log(bomb, numBomb, bomb.length, size)
+    while (bomb.length < 16) {
+        const numBomb = parseInt(Math.random() * numOfGridIt) + 1
+        if (bomb.includes(numBomb) === false) {
+            bomb.push(numBomb)
+        }
+        console.log(bomb, numBomb, bomb.length, size)
 
 
     }
 
 
     gridEl.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+
+    const score = []
 
     for (let i = 0; i < numOfGridIt; i++) {
         const num = i + 1
@@ -44,6 +48,20 @@ function readyP1() {
 
         gridItem.addEventListener('click', function () {
             console.log('click sulla casella', num)
+           if(score.includes(num) === false){
+                score.push(num)
+                if (bomb.includes(num)) {
+                    gridItem.classList.add('bg-red')
+                    console.log("è una bomba")
+                    alert("Hai perso!!!",)
+                } else {
+                    gridItem.classList.add('bg-dark-blue')
+                    console.log("non è una bomba")
+                    
+                    console.log(score.length, "punti")
+                }
+                
+            }
 
             // const { x, y } = getCoordinates(i, size);
 
@@ -51,37 +69,31 @@ function readyP1() {
             // console.log("down", {x: x , y: y + 1})
             // console.log("left", {x: x - 1 , y: y})
             // console.log("right", {x: x + 1 , y: y})
+
+
             
-            
-            if(bomb.includes(num)){
-                gridItem.classList.toggle('bg-red')
-                console.log("è una bomba")
-                alert("Hai perso!!!")
-            }else{
-                gridItem.classList.toggle('bg-dark-blue') 
-                console.log("")
-            }   
         }
         )
     }
 }
 
-function getCoordinates(index, size) {
-    return {
-        x: index % size,
-        y: Math.floor(index / size)
-    }
-}
+// function getCoordinates(index, size) {
+//     return {
+//         x: index % size,
+//         y: Math.floor(index / size)
+//     }
+// }
 
-function getSize (){
+function getSize() {
     let size = 7
     let level = levelSelectEl.value
 
-    if(level === "easy"){
+    if (level === "easy") {
         size = 10
-    } else if(level === "medium"){
+    } else if (level === "medium") {
         size = 9
     }
 
-    return size 
+    return size
 }
+
